2edb71e855c75afe4892120c474a18c859d39312
newenclave <pikrat.acid@gmail.com>
2017-03-07 11:49:45 +0300

    objects

--- main.cpp
+++ main.cpp
@@ -7,29 +7,23 @@
 template <typename T>
 struct sdl_deleter;
 
-template <>
-struct sdl_deleter<SDL_Surface> {
-
-    using value_type    = SDL_Surface;
-    using deleter_type  = sdl_deleter<value_type>;
-
-    void operator ( )(value_type *obj) const
-    {
-        SDL_FreeSurface( obj );
+#define SDL_DECLARE_DELETER( Type, DeleteCall )         \
+    template <>                                         \
+    struct sdl_deleter<Type> {                          \
+                                                        \
+        using value_type    = Type;                     \
+        using deleter_type  = sdl_deleter<value_type>;  \
+                                                        \
+        void operator ( )( value_type *obj ) const      \
+        {                                               \
+            DeleteCall( obj );                          \
+        }                                               \
     }
-};
 
-template <>
-struct sdl_deleter<SDL_Window> {
-
-    using value_type    = SDL_Window;
-    using deleter_type  = sdl_deleter<value_type>;
-
-    void operator ( )( value_type *obj ) const
-    {
-        SDL_DestroyWindow( obj );
-    }
-};
+SDL_DECLARE_DELETER( SDL_Surface,   SDL_FreeSurface );
+SDL_DECLARE_DELETER( SDL_Window,    SDL_DestroyWindow );
+SDL_DECLARE_DELETER( SDL_Renderer,  SDL_DestroyRenderer );
+SDL_DECLARE_DELETER( SDL_Texture,   SDL_DestroyTexture );
 
 template <typename T>
 class sdl_object {
@@ -43,16 +37,16 @@ public:
     sdl_object( )
     { }
 
-    explicit sdl_object( value_type *obj )
+    sdl_object( value_type *obj )
         :ptr_(obj, sdl_deleter<value_type>( ))
     { }
-
-    sdl_object( const sdl_object & ) = delete;
-    sdl_object& operator = ( const sdl_object & ) = delete;
 
     sdl_object( sdl_object &&other )
         :ptr_(std::move(other.ptr_))
     { }
+
+    sdl_object( const sdl_object & )              = delete;
+    sdl_object& operator = ( const sdl_object & ) = delete;
 
     sdl_object& operator = ( sdl_object &&other )
     {
@@ -82,21 +76,76 @@ public:
         return ptr_.get( );
     }
 
+    operator bool ( ) const
+    {
+        return ptr_.get( ) != nullptr;
+    }
+
 private:
 
     object_ptr ptr_;
 };
 
+sdl_object<SDL_Texture> load_text( SDL_Renderer *rndr, const char *path )
+{
+    sdl_object<SDL_Texture> txt;
+    sdl_object<SDL_Surface> res = SDL_LoadBMP( path );
+
+    if( res.get( ) ) {
+        txt = SDL_CreateTextureFromSurface( rndr, res.get( ) );
+    }
+    return std::move(txt);
+}
+
 int main( )
 {
     int res = SDL_Init( SDL_INIT_VIDEO );
 
-    sdl_object<SDL_Window> main_window (
+    sdl_object<SDL_Window> main_window =
             SDL_CreateWindow( "test", SDL_WINDOWPOS_UNDEFINED,
                               SDL_WINDOWPOS_UNDEFINED,
-                              640, 480, SDL_WINDOW_SHOWN ) );
+                              640, 480, SDL_WINDOW_SHOWN );
 
+    sdl_object<SDL_Renderer> renderer =
+            SDL_CreateRenderer( main_window.get( ), -1,
+                                SDL_RENDERER_ACCELERATED );
 
+    SDL_SetRenderDrawColor( renderer.get( ), 0xFF, 0xFF, 0xFF, 0xFF );
+    IMG_Init( IMG_INIT_PNG );
+
+    bool quit = false;
+
+    auto up    = load_text( renderer.get( ), "data/up.bmp" );
+    auto down  = load_text( renderer.get( ), "data/down.bmp" );
+    auto left  = load_text( renderer.get( ), "data/left.bmp" );
+    auto right = load_text( renderer.get( ), "data/right.bmp" );
+
+    auto last = up.get( );
+
+    SDL_Rect rect = { 0, 0, 100, 100 };
+
+    while( !quit ) {
+        SDL_Event e;
+        while( SDL_PollEvent( &e ) ) {
+            if( e.type == SDL_QUIT ) {
+                quit = true;
+                break;
+            } else if( e.type == SDL_KEYDOWN ) {
+                switch (e.key.keysym.sym) {
+                case SDLK_UP:    last = up.get( ); break;
+                case SDLK_DOWN:  last = down.get( ); break;
+                case SDLK_RIGHT: last = left.get( ); break;
+                case SDLK_LEFT:  last = right.get( ); break;
+                default:
+                    break;
+                }
+            }
+
+            SDL_RenderClear( renderer.get( ) );
+            SDL_RenderCopy( renderer.get( ), last, NULL, &rect );
+            SDL_RenderPresent( renderer.get( ) );
+        };
+    }
 
     return 0;
 }
